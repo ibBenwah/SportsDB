222f35e47e76ca7b4c09f5e1ec60d8797de79a3d
ibBenwah <garret.c.benoit@gmail.com>
2023-03-11 18:15:19 -0500

    Add project files.

---
+++ SportsDb/DbContext/LeagueInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SportsDb.DbContext
+{
+    public class LeagueInfo
+    {
+        [Key]
+        public string LeagueId { get; set; }
+        public int NumberOfTeams { get; set; }
+        public string Sport { get; set; }
+    }
+}
